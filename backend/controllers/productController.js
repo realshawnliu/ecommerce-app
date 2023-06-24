4e6d76928a1dbdd9276e0e4606d8cdcfa59328f2
Shawn <realshawnliu@gmai.com>
2023-06-24 16:51:14 +1000

    add errorHandler

---
+++ backend/controllers/productController.js
@@ -0,0 +1,3 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/productModel.js";
+
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -11,6 +12,9 @@ connectDB();
 
 const app = express();
 console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.use("/api/products", productRoutes);
 
