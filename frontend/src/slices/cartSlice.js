76e527938803f1124fc70f2f236382069689ee87
Shawn <realshawnliu@gmail.com>
2023-06-28 17:15:17 +1000

    addToCart button and quantity selector

---
+++ frontend/src/slices/cartSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : { cartItems: [] };
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+
+      const existItem = state.cartItems.find((x) => x._id === item._id);
+
+      if (existItem) {
+        state.cartItems = state.cartItems.map((x) => (x._id === existItem._id ? item : x));
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+
+      state.itemsPrice = addDecimals(
+        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+      );
+
+      state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)));
+
+      state.totalPrice = (
+        Number(state.itemsPrice) +
+        Number(state.shippingPrice) +
+        Number(state.taxPrice)
+      ).toFixed(2);
+
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
+  },
+});
+
+const addDecimals = (num) => {
+  return (Math.round(num * 100) / 100).toFixed(2);
+};
+
+export default cartSlice.reducer;
+export const { addToCart } = cartSlice.actions;
