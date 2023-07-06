9833661af99fa5f21f94f48f18e15c7db1166a4e
Shawn <realshawnliu@gmail.com>
2023-07-06 16:31:53 +1000

    add authMiddleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import userRoutes from "./routes/userRoute.js";
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -10,9 +12,14 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
